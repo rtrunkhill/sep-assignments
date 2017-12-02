b777ed0bbeaf0843af2f1d2ab75d0c02b3452ff6
Rory Trunkhill <rtrunkhill@gmail.com>
2017-12-02 21:37:22 +0000

    hashes1 complete

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -6,16 +6,33 @@ class HashClass
   end
 
   def []=(key, value)
-    # hash  = @item[index(key,@size)]
+    hash = @items[index(key, @size)]
+    if hash.nil?
+      @items[index(key, @size)] = HashItem.new(key, value)
+    elsif hash.key != key
+      while @items[index(key, @size)].key != nil && @items[index(key, @size)].key != key
+        resize
+        new_index = index(key, @size)
+        break if @items[new_index] == nil
+      end
+      self[key] = value
+    elsif hash.key == key && hash.value != value
+      resize
+      hash.value = value
+    end
   end
-
+ 
 
   def [](key)
+    item = @items[index(key, @size)]
+    item.nil? ? nil : item.value
   end
 
   def resize
-   @size.times { @items << nil }
-   @size = @size * 2
+    temp = @items.compact
+    @size = @size * 2
+    @items = Array.new(@size)
+    temp.each { |i| @items[index(i.key, @size)] = i }
   end
 
   # Returns a unique, deterministically reproducible index into an array
