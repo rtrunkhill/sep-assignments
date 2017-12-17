225c89a5942c6ae99af5e859c0665a6cfda22978
Rory Trunkhill <rtrunkhill@gmail.com>
2017-12-17 23:23:35 +0000

    just gettig started

---
+++ 01-data-structures/07-graphs/kevin_bacon.rb
@@ -0,0 +1,10 @@
+require_relative 'node'
+
+class KevinBacon
+
+    def find_kevin_bacon(star)
+        films = []
+    end
+end
+
+# Kevin_Bacon.film_actor_hash['Footloose'] = [Lori_Singer, John_Lithgow, Dianne_Wiest, Chris_Penn, Sarah_Jessica_Parker, …]
