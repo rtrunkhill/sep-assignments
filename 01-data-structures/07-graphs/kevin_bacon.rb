988261d138388a142cc38e52b71f8d53635bd109
Rory Trunkhill <rtrunkhill@gmail.com>
2017-12-18 18:06:35 +0000

    whatever

--- 01-data-structures/07-graphs/kevin_bacon.rb
+++ 01-data-structures/07-graphs/kevin_bacon.rb
@@ -1,10 +1,21 @@
 require_relative 'node'
 
+# Kevin_Bacon.film_actor_hash['Footloose'] = [Lori_Singer, John_Lithgow, Dianne_Wiest, Chris_Penn, Sarah_Jessica_Parker, …]
+
+# DEF FIND_PATH(Node start, Node end)
+#   IF start is equal to end THEN
+#     RETURN end
+#   END IF
+#   FOR each node that start connects to
+#     IF the node is closer to end than start THEN
+#       RETURN start + CALL FIND_PATH(node, end)
+#     END IF
+#   END FOR
+# END DEF
+
 class KevinBacon
 
     def find_kevin_bacon(star)
         films = []
     end
 end
-
-# Kevin_Bacon.film_actor_hash['Footloose'] = [Lori_Singer, John_Lithgow, Dianne_Wiest, Chris_Penn, Sarah_Jessica_Parker, …]
