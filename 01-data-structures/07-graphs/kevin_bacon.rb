d07dc48b93ff3fdba642ed229a3d10c5a44c3f01
Rory Trunkhill <rtrunkhill@gmail.com>
2017-12-26 20:37:30 +0000

    rspec hooked up?

--- 01-data-structures/07-graphs/kevin_bacon.rb
+++ 01-data-structures/07-graphs/kevin_bacon.rb
@@ -4,13 +4,15 @@ class KevinBacon
 
     def find_kevin_bacon(star)
         films = []
+        p "films: #{films}"
         if films.length > 6
             return "No Bacon here!"
         else
             star.film_actor_hash.each do |movie, actors|
                 films << movie unless films.include?(movie)
                 actors.each do |hero|
-                    hero == kevin_bacon ? (return films) : find_kevin_bacon(hero)
+                    hero.name == "Kevin Bacon"  ? (return films) : find_kevin_bacon(hero)
+                    #fix the hero ==kevin bacon node
                 end
             end
         end
--- 01-data-structures/07-graphs/kevin_bacon_spec.rb
+++ 01-data-structures/07-graphs/kevin_bacon_spec.rb
@@ -6,7 +6,7 @@ require_relative 'node'
 RSpec.describe KevinBacon, type: Class do
     # kevin_bacon.film_actor_hash['Footloose'] = [Lori_Singer, John_Lithgow, Dianne_Wiest, Chris_Penn, Sarah_Jessica_Parker, …]
     
-    # let (:graph) { KevinBacon.new(star) }
+    let (:graph) { KevinBacon.new() }
     let (:kevin_bacon) { Node.new("Kevin Bacon") }
     let (:chuck) { Node.new("Chuck") }
     let (:paul) { Node.new("Paul") }
@@ -17,19 +17,19 @@ RSpec.describe KevinBacon, type: Class do
     # let (:mitch) { Node.new("Mitch") }
     # let (:nancy) { Node.new("Nancy") }
     # let (:michelle) {Node.new("Michelle") }
-
-    kevin_bacon.film_actor_hash['Footloose'] = [donald, chuck]
-    kevin_bacon.film_actor_hash['A Few Good Men'] = [hillary]
-    chuck.film_actor_hash['Footloose'] = [kevin_bacon, donald]
-    chuck.film_actor_hash['Glory'] = [donald]
-    donald.film_actor_hash['Footloose'] = [chuck, kevin_bacon]
-    donald.film_actor_hash['Glory'] = [chuck]
-    donald.film_actor_hash['Bill and Teds'] = [paul]
-    hillary.film_actor_hash['A Few Good Men'] = [kevin_bacon]
-    hillary.film_actor_hash['Star Wars'] = [paul]
-    paul.film_actor_hash['Star Wars'] = [hillary]
-    paul.film_actor_hash['Bill and Teds'] = [donald]
-    
+    before do
+        kevin_bacon.film_actor_hash['Footloose'] = [donald, chuck]
+        kevin_bacon.film_actor_hash['A Few Good Men'] = [hillary]
+        chuck.film_actor_hash['Footloose'] = [kevin_bacon, donald]
+        chuck.film_actor_hash['Glory'] = [donald]
+        donald.film_actor_hash['Footloose'] = [chuck, kevin_bacon]
+        donald.film_actor_hash['Glory'] = [chuck]
+        donald.film_actor_hash['Bill and Teds'] = [paul]
+        hillary.film_actor_hash['A Few Good Men'] = [kevin_bacon]
+        hillary.film_actor_hash['Star Wars'] = [paul]
+        paul.film_actor_hash['Star Wars'] = [hillary]
+        paul.film_actor_hash['Bill and Teds'] = [donald]
+    end
     # film_actor_hash['Footloose'] = [donald, kevin_bacon, chuck]
     # film_actor_hash['A Few Good Men'] =[hillary, kevin_bacon] 
     # film_actor_hash['Glory'] = [chuck, donald]
@@ -59,7 +59,9 @@ RSpec.describe KevinBacon, type: Class do
     describe "#find_kevin_bacon" do
         it "checks if start node == kevin_bacon" do
             graph.find_kevin_bacon(kevin_bacon)
-            expect(films).to eq "Footloose"
+            # p "*****************"
+            # p "films: #{films}"
+            # expect(films).to eq ['Footloose']
         end
         
         # it "returns films array if start node != kevin_bacon"
